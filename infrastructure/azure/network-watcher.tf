3750f3d1d7b66ab815be3a2538e8ac5605be8100
Jim Counts <jamesrcounts@outlook.com>
2021-06-08 12:22:05 -0400

    Disable network watcher

--- infrastructure/azure/network-watcher.tf
+++ infrastructure/azure/network-watcher.tf
@@ -1,4 +1,4 @@
-data "azurerm_network_watcher" "example" {
-  name                = "NetworkWatcher_centralus"
-  resource_group_name = "NetworkWatcherRG"
-}
+// data "azurerm_network_watcher" "example" {
+//   name                = "NetworkWatcher_centralus"
+//   resource_group_name = "NetworkWatcherRG"
+// }
